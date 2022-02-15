6d0a28980008579b46f88f231b4ee424b005f534
Julien-Perrotgit config --global user.email p.julien.perrot@hotmail.com <p.julien.perrot@hotmail.com>
2022-02-15 08:59:20 +0100

    Learning Bootstrap

---
+++ script.js
@@ -0,0 +1,7 @@
+// Pour mes TOOLTIPS dans mon html, récuper dans BOOTSTRAP :
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
